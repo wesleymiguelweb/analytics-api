d2e2c45a68a5b8607763f63c7dee21e920c93c80
Wesley Miguel <wesley.miguel7@outlook.com>
2026-04-14 20:42:46 -0300

    Update AnalyticsApiApplication.java

--- src/main/java/com/growthmachine/analytics/AnalyticsApiApplication.java
+++ src/main/java/com/growthmachine/analytics/AnalyticsApiApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(
         info = @Info(
-                title = "API Marketing de Crescimento",
+                title = "Marketing de Crescimento",
                 version = "2.0.0",
-                description = "API RESTful desenvolvida para gestão de campanhas de tráfego pago, contas de anunciantes e análise de métricas diárias.",
+                description = "API desenvolvida para gestão de campanhas de tráfego pago, contas de anunciantes e análise de métricas diárias.",
                 contact = @Contact(
                         name = "Wesley Miguel",
                         url = "https://github.com/wesleymiguelweb"
